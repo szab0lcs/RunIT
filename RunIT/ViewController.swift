18b50db2947b146a28f77a9aa4034811a0e1238a
Szabolcs Kencse <szabolcskencse@gmail.com>
2018-11-26 13:31:12 +0200

    Initial Commit

---
+++ RunIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunIT
+//
+//  Created by Szabolcs Kencse on 26/11/2018.
+//  Copyright © 2018 Szabolcs Kencse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RunITTests/RunITTests.swift
@@ -0,0 +1,34 @@
+//
+//  RunITTests.swift
+//  RunITTests
+//
+//  Created by Szabolcs Kencse on 26/11/2018.
+//  Copyright © 2018 Szabolcs Kencse. All rights reserved.
+//
+
+import XCTest
+@testable import RunIT
+
+class RunITTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
